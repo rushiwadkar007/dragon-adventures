7ee3e25b2bc7c772a9b6f1f8515fff41a5b34e1d
rushiwadkar007 <rwadkar920@gmail.com>
2021-03-14 14:58:14 +0530

    Javascript dragon game project-first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+score = 0;
+cross = true;
+audiogo = new Audio('gameover.mp3');
+audio = new Audio('music.mp3');
+setTimeout(() => {
+    audio.play();
+}, 1000);
+document.onkeydown = function(e){
+    console.log("Key code is: ", e.key);
+    if(e.key == 'ArrowUp'){
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(()=>{
+            dino.classList.remove('animateDino')
+        }, 2000);
+    }
+    if(e.key == 'ArrowRight'){
+        dino = document.querySelector('.dino');
+        dinox = parseInt( window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinox + 56) +"px";
+    }
+    if(e.key == 'ArrowLeft'){
+        dino = document.querySelector('.dino');
+        dinox = parseInt( window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinox - 56) +"px";
+    }
+}
+
+setInterval(()=>{ 
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt( window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx-ox);
+    offsetY = Math.abs(dy-oy);
+
+    if(offsetX <73 && offsetY< 52 ){
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obstacleAni');
+        audiogo.play();
+        setTimeout(() => {
+            audiogo.pause();
+            audio.pause();
+        }, 1000);
+    }
+    else if(offsetX< 145 && cross){
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(()=>{
+            cross = true;
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's';
+        }, 2500);
+        
+    }
+}, 10);
+
+function updateScore(score){ 
+    scoreCont.innerHTML = "Your Score is: " +score;
+}
